70e54523b70017577776000233c24259cb8e22f0
slavik-gassiev <slavikgassiev1872002@gmail.com>
2024-06-01 14:10:29 +0300

    Added DAO pattern.

--- src/main/java/com/slava/learn/currency_exchanger/ConnectionDB.java
+++ src/main/java/com/slava/learn/currency_exchanger/ConnectionDB.java
@@ -1,38 +1,26 @@
 package com.slava.learn.currency_exchanger;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 public class ConnectionDB {
-    private Connection connection;
+    private static final HikariDataSource HIKARI_DATA_SOURCE;
 
-    public ConnectionDB() {
-        ConnectDB();
+    static {
+        HikariConfig hikariConfig = new HikariConfig();
+
+        hikariConfig.setJdbcUrl("jdbc:sqlite:mydb.db");
+        hikariConfig.setDriverClassName("org.sqlite.JDBC");
+
+        HIKARI_DATA_SOURCE = new HikariDataSource(hikariConfig);
     }
 
-    public void ConnectDB() {
-        String url = "jdbc:sqlite:mydb.db";
-        try {
-            connection = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static Connection getConnection() throws SQLException {
+        return HIKARI_DATA_SOURCE.getConnection();
     }
-
-    public Connection getConnection() {
-        return connection;
-    }
-
-    public void closeConnection() {
-        try {
-            if(connection != null) {
-                connection.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
